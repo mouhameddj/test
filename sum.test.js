0de618e5ea7e1540bdd51932b944af28af83648f
mouhameddj <93831030+mouhameddj@users.noreply.github.com>
2022-12-11 21:26:13 +0100

    Update sum.test.js

--- sum.test.js
+++ sum.test.js
@@ -2,8 +2,8 @@ const sum = require("./sum")
 
 describe(
     'test sum',()=>{
-        test('test 2-1 = 1', () => {
-            expect(sum(2,1)).toBe(1)
+        test('test 2+2 = 4', () => {
+            expect(sum(2,2)).toBe(4)
          })
     }
 )
